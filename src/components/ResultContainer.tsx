bb17719eb154e25f00b5dd31670dded38bf4ed74
robin-sevelin <robinsevelin@hotmail.com>
2023-09-19 10:10:50 +0200

    bug fix

--- src/components/ResultContainer.tsx
+++ src/components/ResultContainer.tsx
@@ -1,8 +1,3 @@
 export const ResultContainer = () => {
-  return (
-    <>
-      {/* <OccupationsList />
-      <EnrichedOccupations /> */}
-    </>
-  );
+  return <>{/* <OccupationsList /> */}</>;
 };
