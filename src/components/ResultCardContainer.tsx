6a653710d0bcb32628b5c7712f1d340c80c462d2
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2023-09-19 23:26:29 +0200

    Logic for AboutEdu component

--- src/components/ResultCardContainer.tsx
+++ src/components/ResultCardContainer.tsx
@@ -1,23 +1,43 @@
-import { useContext } from "react"
-import { SearchContext } from "../contexts/SearchContext"
+import { useContext } from "react";
+import { SearchContext } from "../contexts/SearchContext";
 import { DigiButton } from "@digi/arbetsformedlingen-react";
+import { IEducation } from "../models/IEducation";
+import { getEducationById } from "../services/DataService";
 
-export const ResultCardContainer = () => {
-    const searchResult = useContext(SearchContext);
-    const foundEducations = searchResult.search.educations;
-    
-    console.log(foundEducations[0]);
-    
-    return(
-        <div className="cardContainer">
-            {foundEducations.map((education) => (
-                <div className="eduCard" key={education.education.identifier}>
-                    <h4>{education.education.title[0].content} - {education.education.code}</h4>
-                    <p>{education.education.form.code}, {education.education.configuration.code}</p>
-                    <DigiButton>Relaterade Yrken</DigiButton>
-                    <DigiButton>Om Utbildningen</DigiButton>
-                </div>
-            ))}
-        </div>
-    )
+interface ResultCardContainerProps {
+  selectedEducation: IEducation | null;
+  setSelectedEducation: (education: IEducation | null) => void;
 }
+
+export const ResultCardContainer = ({ setSelectedEducation }: ResultCardContainerProps) => {
+  const searchResult = useContext(SearchContext);
+  const foundEducations = searchResult.search.educations;
+
+  const handleEducationClick = async (id: string) => {
+    try {
+      const response = await getEducationById(id);
+      setSelectedEducation(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="cardContainer">
+      {foundEducations.map((education) => (
+        <div className="eduCard" key={education.education.identifier}>
+          <h4>
+            {education.education.title[0].content} - {education.education.code}
+          </h4>
+          <p>
+            {education.education.form.code}, {education.education.configuration.code}
+          </p>
+          <DigiButton onClick={() => handleEducationClick(education.id)}>
+            Om Utbildningen
+          </DigiButton>
+          <DigiButton>Relaterade yrken</DigiButton>
+        </div>
+      ))}
+    </div>
+  );
+};
