e90d0c75ab48455870a59c020107200282fcfe53
Emilia <emiliamassings@gmail.com>
2023-09-19 12:16:15 +0200

    Added loader component

---
+++ src/components/ShowLoader.tsx
@@ -0,0 +1,9 @@
+import { DigiLoaderSpinner } from "@digi/arbetsformedlingen-react"
+
+export const ShowLoader = () => {
+    return (
+        <div className="loaderContainer">
+            <DigiLoaderSpinner></DigiLoaderSpinner>
+        </div>
+    )
+}
