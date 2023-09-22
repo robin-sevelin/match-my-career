cdda75c835af8614436295997196e96477ddcec4
Emilia <emiliamassings@gmail.com>
2023-09-22 11:11:17 +0200

    Installed scss

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '@digi/arbetsformedlingen/dist/digi-arbetsformedlingen/digi-arbetsformedlingen.css';
-import './App.css';
+import './style/_app.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
