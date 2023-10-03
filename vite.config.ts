195152011335d43e612f279f2a9b9f48560b35d3
robin-sevelin <robinsevelin@hotmail.com>
2023-10-03 17:01:39 +0200

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/match-my-career/',
   optimizeDeps: {
     exclude: ['@digi/arbetsformedlingen'],
   },
+  base: '/match-my-career/',
   plugins: [react()],
 });
